9262276a56bdb833f2584c17d873ce63fe49de06
n0tOdd <oddostlie@gmail.com>
2025-05-27 22:04:00 +0200

    Made it into a integration, Installs the needed files in "/config/www/jukebox" and uses http://homeassistant.local:8123/local/jukebox/jukebox.html" as default url Also Installs a controller blueprint that you need to setup after the integration
    Made some changes to the html file to work with the integration

---
+++ custom_components/music_assistant_jukebox/const.py
@@ -0,0 +1,15 @@
+"""Constants for music_assistant_jukebox."""
+
+from logging import Logger, getLogger
+import os
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "music_assistant_jukebox"
+CONF_MUSIC_ASSISTANT_ID = "music_assistant_id"
+CONF_MEDIA_PLAYER = "media_player"
+
+# File paths
+WWW_JUKEBOX_DIR = "www/jukebox"
+TOKEN_FILE = f"{WWW_JUKEBOX_DIR}/jukeboxtoken.key"
+HTML_FILE = f"{WWW_JUKEBOX_DIR}/jukebox.html"
+BLUEPRINT_FILE = "blueprints/automation/music_assistant_jukebox/jukebox_controller.yaml"
